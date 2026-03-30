368cc155b40c7b1df6c69eb2747e9fe8892e0e56
Daniel Pelzel <pelzel.daniel@gmail.com>
2026-03-31 00:25:38 +0200

    made GUI work

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,38 +1,87 @@
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
-ser = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=1)  # open serial port
+matplotlib.use("QT5Agg")
+import time
 
-print("Lese Daten ... ")
 
-#Plot erstellen
+ser = serial.Serial("/dev/cu.usbmodem14201", 9600, timeout=0.1)
+time.sleep(2)
+
 plt.ion()
-fig = plt.figure()
+fig = plt.figure(figsize=(8, 8), dpi=100)
 ax = fig.add_subplot(111, projection='polar')
-ax.set_ylim(0, 400)
-ax.set_xlim(0, 145*np.pi/180)
 
-angle = 0
+#Backend Design
+fig.patch.set_facecolor('black')
+ax.set_facecolor('black')
+ax.tick_params(colors = "lime")
+ax.grid(color = "lime", alpha=0.3)
+ax.set_title("Radar", color = "lime", fontsize = 14 )
 
-beam, = ax.plot([0,0], [0,400], color = "g", lw=2)
+points = []
 
-while True:
-    try:
-        line = ser.readline().decode("utf-8", errors="ignore").strip()
+
+
+ax.set_ylim(0,400)
+ax.set_theta_zero_location("W")
+ax.set_theta_direction(-1)
+ax.set_thetalim(0, np.pi)
+ax.set_rticks([0, 100, 200, 300, 400])
+
+beam_bg, = ax.plot([0,0], [0, 400], color = "lime", lw=3)
+beam_obj, = ax.plot([0,0], [0, 0], color = "red", lw=3)
+
+plt.show()
+plt.pause(0.001)
+
+print("Lese Daten ...")
+
+
+ser.reset_input_buffer()
+try:
+    while True:
+
+        line = ser.readline().decode("utf-8", errors = "ignore").strip()
+
+        if line:
+            print("data: " + line)
 
         if line and "," in line:
-            parts  = line.split(",")  #creates a list of Data read from the serial port (angle and distance)
-            angle_rad = np.deg2rad(int(parts[0]))  #angle in radiant
-            distance = float(parts[1]) #distance in cm
 
-            beam.set_data([angle_rad, angle_rad], [0, distance])
-            plt.pause(0.0001)
-    except ValueError:
-        continue
+            parts = line.split(",")
 
-    except KeyboardInterrupt:
-        print("\nProgramm beendet")
+            if len(parts) == 2:
+
+                    theta = np.deg2rad(float(parts[0]))
+                    r = float(parts[1])
+
+                    beam_bg.set_data([theta, theta], [0, 400])
+                    beam_obj.set_data([theta, theta], [0, r])
+
+                    dot, = ax.plot(theta, r, "o", color="r", markersize=4)
+                    points.append((time.time(), dot))
+
+                    neue_liste = []
+                    for t, d in points:
+                        if time.time() - t < 2.0:
+                            neue_liste.append((t, d))  # jung → behalten
+                        else:
+                            d.remove()  # alt → aus Plot löschen
+                    points = neue_liste
 
 
+                    # Diese drei Zeilen wecken das Fenster auf:
+                    fig.canvas.draw_idle()
+
+
+                    fig.canvas.flush_events()
+                    plt.pause(0.01)
+except ValueError:
+    print("Fehler beim Parsen der Daten.")
+finally:
+    ser.close()
+    print("Serielle Schnittstelle geschlossen.")
 
